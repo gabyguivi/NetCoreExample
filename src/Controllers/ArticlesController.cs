33c9992761d4184aabec9a615ace08d59b8b9ef6
Guivi <gguivisdalsky@accessinformatica.com.ar>
2019-09-15 11:52:43 -0300

    primer commit

---
+++ src/Controllers/ArticlesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using netCoreWorkshop.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace netCoreWorkshop.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(Article.DataSource);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Article.DataSource.RemoveAll(a => a.Id == id);
+            return View("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            return View(Article.DataSource.First(a => a.Id == id));
+        }
+
+        [HttpGet]
+        public IActionResult Detail(int id)
+        {
+            return View(Article.DataSource.First(a => a.Id == id));
+        }
+
+        [HttpPost]
+        public IActionResult Create(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                article.Id = Article.DataSource.Count() + 1;
+                Article.DataSource.Add(article);
+                return RedirectToAction("Index");
+            }
+
+            return View(article);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                Article.DataSource.First(a=>a.Id== article.Id).Title = article.Title;
+                return RedirectToAction("Index");
+            }
+
+            return View(article);
+        }
+
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace netCoreWorkshop
+{
+    class Program
+    {
+        // Minimal ASP.NET setup in Main method
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+    }
+}
